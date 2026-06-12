acb6cfa9edeb9722daf73b3edab187a856ebd4f0
olisettythanush-cell <olisettythanush@gmail.com>
2026-06-12 17:06:36 +0530

    Create AllCraftable mod class with initialization message

---
+++ src/main/java/com/username/allcraftable/AllCraftable.java
@@ -0,0 +1,11 @@
+package com.yourname.allcraftable;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("allcraftable")
+public class AllCraftable {
+
+    public AllCraftable() {
+        System.out.println("AllCraftable mod loaded!");
+    }
+}
